328bfeb6b8a6ae7db434db082bcd670bc935fa4f
Sean Strauss <seandstrauss@gmail.com>
2015-10-15 16:05:44 -0400

    finished services for application

---
+++ underwritingApp/app/features/services/services.js
@@ -0,0 +1,47 @@
+var services = angular.module('applicant.services', []);
+services.factory('applicantServices', function($http) {
+
+
+	$http({
+		method: 'GET',
+		url: 'http://www.w3schools.com/angular/customers.php'
+
+	}).then(function successCallback(response) {
+		// this callback will be called asynchronously
+		// when the response is available
+		creditData = response.data;
+		//console.log(cardData);
+
+
+
+		//console.log(response)
+	});
+
+	$http({
+		method: 'POST',
+		url: 'http://www.w3schools.com/angular/customers.php'
+		}).then(function successCallback(response) {
+		// this callback will be called asynchronously
+		// when the response is available
+		postResponse = response.data;
+		//console.log(cardData);
+
+
+
+		//console.log(response)
+	});
+
+	return {
+		getAll: function() {
+			for (key in creditData) {
+				var creditData = creditInfo[key];
+			}
+			return creditData;
+		}
+		getPostResponse: function(){
+			for (key in postResponse){
+				var postData = postResponse[key];
+			}
+			return postResponse;
+		}
+});
