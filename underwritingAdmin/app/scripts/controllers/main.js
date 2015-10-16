68d4e580b8baeb1594da579a4c369995eb61c004
Smashmantha <skraft@Zips-MacBook-Pro-8.local>
2015-10-16 14:20:55 -0400

    admin ctrl, form and image

--- underwritingAdmin/app/scripts/controllers/main.js
+++ underwritingAdmin/app/scripts/controllers/main.js
@@ -8,10 +8,16 @@
  * Controller of the underwritingAdminApp
  */
 angular.module('underwritingAdminApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
-    ];
+   
+   ];
+
+   		$scope.submit = function(isValid){
+   			console.log($scope.admin);
+   		}
+   
   });
